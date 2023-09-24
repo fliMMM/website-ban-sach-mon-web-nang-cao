9ebec1d14407b887d47640b5c58baa05abb17f1f
Amaru222 <letoanduc22@gmail.com>
2023-09-25 04:24:44 +0700

    duc

--- resources/views/components/sectiontitle.blade.php
+++ resources/views/components/sectiontitle.blade.php
@@ -1,20 +1,26 @@
 <?php
 $books = array(
-        array('id' => '1', 'img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg' ),
-        array('id' => '2','img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg'),
-        array('id' => '3','img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg'),
-        array('id' => '4', 'img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg'),
-        array('id' => '5', 'img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg'),
+        array('id' => '1', 'img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg', 'price' => '60000'),
+        array('id' => '2','img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg',  'price' => '60000'),
+        array('id' => '3','img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg',  'price' => '60000'),
+        array('id' => '4', 'img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg',  'price' => '60000'),
+        array('id' => '5', 'img' => 'https://product.hstatic.net/200000343865/product/33_c41b6baaa61d479f9846d9f65d09e717_large.jpg',  'price' => '60000'),
     );
 ?>
 <div>
     <div class="flex justify-center">
         <h3 class="mt-16 ">Sách mới</h3>
     </div>
+ 
     <div class="flex justify-center">
         @foreach ($books as $book)
-                 <img src="{{$book['img']}}" alt="" class="w-48 h-72 m-4 "> 
-        @endforeach
+        <div>
+            <img src="{{$book['img']}}" alt="" class="w-48 h-72 ml-4 mt-4 mr-4 "> 
+            <p class = " ml-4 " style="color: #d51c24">{{$book['price']}}</p>
+        </div>  
+        @endforeach    
     </div>
+      
+
     <a href="" class="flex justify-end text-red-600 no-underline">Xem thêm >>></a>
 </div>
