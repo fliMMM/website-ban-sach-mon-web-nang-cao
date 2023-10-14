4f6ba52e9307459576536c9c5545c57577072f1f
khangpham202 <doangiang2612@gmail.com>
2023-10-14 20:59:03 +0700

    update filtered in collection  and admin page

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Categories;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        $userCount = User::count();
+        $productCount = Product::count();
+        $categoryCount = Categories::count();
+        // $orderCount = Order::count();
+        return view('admin.dashboard', compact('userCount', 'productCount', 'categoryCount',));
+    }
+
+    public function manageProducts()
+    {
+        $products =  Product::paginate(5);
+        return view('admin.product', compact('products'));
+    }
+    public function editProduct($id)
+    {
+        $product = Product::find($id);
+
+        return response()->json($product);
+    }
+}
